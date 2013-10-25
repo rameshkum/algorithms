f04ad03b9c5e209f2b2c2700974b27ba2bd28bd8
maja <maja>
2013-10-25 23:52:05 +0200

    pow solution

--- Pow.java
+++ Pow.java
@@ -4,14 +4,21 @@ public class Pow {
 
 	public static double pow(double x, int n) {
         // Note: The Solution object is instantiated only once and is reused by each test case.
-        if (n == 0) return x;
-        if (n <0) return 1/pow(x,-n,n); 
-        return pow(x,n,n);
+		 if (n == 0 || x == 1) return 1;
+	        if (Math.abs(x) < 0.0001) return 0;
+	        int i=2;
+	        double solution = (n<0) ? 1/x : x;
+	        int z = Math.abs(n);
+	        while (i<=z) {
+	            if (n<0) {
+	                solution*=1/x;
+	            } else {
+	                solution*=x;
+	            }
+	            i++;
+	        }
+	        return solution;
     }
-    
-    private static double pow(double x, int n, int i) {
-    	if (i==1) return x;
-        return x*pow(x,n,--i);
-    }
+
     
 }
